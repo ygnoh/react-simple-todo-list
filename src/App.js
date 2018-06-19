f6408ec5266ba7e911341d5a6329929503af0e1b
yonggoonoh <nohyg90@gmail.com>
2018-06-20 00:13:44 +0900

    Manage input value as a component state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import TodoList from './TodoList';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ''
+    };
+  }
+
   render() {
     return (
       <React.Fragment>
         <TodoList />
 
         <form>
-          <input type="text" />
+          <input type='text' value={this.state.value} onChange={this.handleChangeValue} />
           <button>submit</button>
         </form>
       </React.Fragment>
     );
   }
+
+  handleChangeValue = event => {
+    this.setState({value: event.target.value});
+  };
 }
 
 export default App;
